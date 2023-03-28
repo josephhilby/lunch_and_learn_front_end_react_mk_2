bd9c8f36a766dae57e3517d37d17fa8f84ddb74c
Joseph Hilby <joseph.hilby@gmail.com>
2023-03-28 10:44:11 -0500

    add fixtures

---
+++ src/fixtures/mockReturnFavorite.js
@@ -0,0 +1,36 @@
+export default function MockFavorite() {
+  return ({
+    "data": [
+      {
+        "id": "1",
+        "type": "favorite",
+        "attributes": {
+          "recipe_title": "Recipe: Egyptian Tomato Soup",
+          "recipe_link": "http://www.thekitchn.com/recipe-egyptian-tomato-soup-weeknight....",
+          "country": "egypt",
+          "created_at": "2022-11-02T02:17:54.111Z"
+        }
+      },
+      {
+        "id": "2",
+        "type": "favorite",
+        "attributes": {
+          "recipe_title": "Crab Fried Rice (Khaao Pad Bpu)",
+          "recipe_link": "https://www.tastingtable.com/entry_detail/chefs_recipes/12214/Fried_rice_with_a_welcome_addition.htm",
+          "country": "thailand",
+          "created_at": "2022-11-07T03:44:08.917Z"
+        }
+      },
+      {
+        "id": "3",
+        "type": "favorite",
+        "attributes": {
+          "recipe_title": "Andy Ricker's Naam Cheuam Naam Taan Piip (Palm Sugar Simple Syrup)",
+          "recipe_link": "https://www.seriouseats.com/recipes/2013/11/andy-rickers-naam-cheuam-naam-taan-piip-palm-sugar-simple-syrup.html",
+          "country": "thailand",
+          "created_at": "2022-11-07T03:44:08.917Z"
+        }
+      }
+    ]
+  })
+}
---
+++ src/fixtures/mockReturnLearningResource.js
@@ -0,0 +1,79 @@
+export default function MockLearningResources() {
+  return (
+    {
+      "data": {
+        "id": null,
+        "type": "learning_resource",
+        "attributes": {
+          "country": "Thailand",
+          "video": {
+            "title": "A Super Quick History of Indonesia",
+            "youtube_video_id": "U2YMewwotiQ"
+          },
+          "images": [
+            {
+              "alt_tag": "Man Standing Outside a Prayer Temple",
+              "url": "https://images.pexels.com/photos/3393546/pexels-photo-3393546.jpeg"
+            },
+            {
+              "alt_tag": "Boats on Seashore",
+              "url": "https://images.pexels.com/photos/3732494/pexels-photo-3732494.jpeg"
+            },
+            {
+              "alt_tag": "Peron Praying at Buddha For Good luck",
+              "url": "https://images.pexels.com/photos/3243020/pexels-photo-3243020.jpeg"
+            },
+            {
+              "alt_tag": "Palm Trees",
+              "url": "https://images.pexels.com/photos/2271198/pexels-photo-2271198.jpeg"
+            },
+            {
+              "alt_tag": "Black Temple With Forest Background",
+              "url": "https://images.pexels.com/photos/130159/pexels-photo-130159.jpeg"
+            },
+            {
+              "alt_tag": "2 Brown Elephants on Brown Sand",
+              "url": "https://images.pexels.com/photos/4012567/pexels-photo-4012567.jpeg"
+            },
+            {
+              "alt_tag": "Group of Elephants Leaning on Tree",
+              "url": "https://images.pexels.com/photos/4012568/pexels-photo-4012568.jpeg"
+            },
+            {
+              "alt_tag": "A Person Standing on the Rocky Mountain",
+              "url": "https://images.pexels.com/photos/5608211/pexels-photo-5608211.jpeg"
+            },
+            {
+              "alt_tag": "The Big Buddha Under Blue Sky",
+              "url": "https://images.pexels.com/photos/6027587/pexels-photo-6027587.jpeg"
+            },
+            {
+              "alt_tag": "Aerial View of Beach Waves on Shore",
+              "url": "https://images.pexels.com/photos/6065588/pexels-photo-6065588.jpeg"
+            },
+            {
+              "alt_tag": "The Buddha Head Embedded on Banyan Tree Roots in Ayutthaya, Thailand",
+              "url": "https://images.pexels.com/photos/6018941/pexels-photo-6018941.jpeg"
+            },
+            {
+              "alt_tag": "Beautiful Beach With Calm Blue Waters ",
+              "url": "https://images.pexels.com/photos/4679438/pexels-photo-4679438.jpeg"
+            },
+            {
+              "alt_tag": "Women Dancing on the Stage",
+              "url": "https://images.pexels.com/photos/4682328/pexels-photo-4682328.jpeg"
+            },
+            {
+              "alt_tag": "Boat on Body of Water",
+              "url": "https://images.pexels.com/photos/4206646/pexels-photo-4206646.jpeg"
+            },
+            {
+              "alt_tag": "Brown and White Temple Under Blue Sky",
+              "url": "https://images.pexels.com/photos/236517/pexels-photo-236517.jpeg"
+            }
+          ]
+        }
+      }
+    }
+  )
+}
