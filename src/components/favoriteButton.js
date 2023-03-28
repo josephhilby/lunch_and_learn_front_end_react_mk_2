9be1f3cbe064eff7e949d7a78b1ab58fe183b162
Joseph Hilby <joseph.hilby@gmail.com>
2023-03-28 10:52:29 -0500

    Add assets

--- src/components/favoriteButton.js
+++ src/components/favoriteButton.js
@@ -1,9 +1,8 @@
-import Heart from "react-animated-heart";
 import { useState } from "react";
 
 export default function FavoriteButton(props) {
   const [isClick, setClick] = useState(false)
   return(
-    <Heart isClick={isClick} onClick={() => setClick(!isClick)}/>
+    <div/>
   )
 }
