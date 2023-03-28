d04177f7ff5fdce069ff3b32d20dd9ffa6b19446
Joseph Hilby <joseph.hilby@gmail.com>
2023-03-28 10:49:41 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./pages/root";
+import Dashboard from "./pages/dashboard";
+import Country from './pages/country';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      },
+      {
+        path: "/country/:name",
+        element: <Country />
+      },
+    ],
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
